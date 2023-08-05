5cd67386fe46771be20e45dc41c3c95702faaeaf
Ekaterina Loshakova <katyushka8905@yandex.ru>
2023-08-05 20:41:19 +0300

    fix5

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,22 +1,30 @@
-const Card = require('../models/card');
+const {
+  badRequestStatus,
+  notFoundStatus,
+  serverErrorStatus,
+} = require("../utils/constants");
+const Card = require("../models/card");
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      Card.findById(card._id)
-        .populate('owner')
-        .then((data) => res.status(201).send(data))
-        .catch(() => res.status(404).send('Карточка с таким _id не найдена'));
+      res.status(201).send(card);
+      // Card.findById(card._id)
+      //   .populate("owner")
+      //   .then((data) => res.status(201).send(data))
+      //   .catch(() =>
+      //     res.status(notFoundStatus).send("Карточка с таким _id не найдена")
+      //   );
     })
     .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res.status(400).send({
+      if (error.name === "ValidationError") {
+        res.status(badRequestStatus).send({
           message: error.message,
         });
       } else {
-        res.status(500).send({
-          message: 'Произошла ошибка на сервере.',
+        res.status(serverErrorStatus).send({
+          message: "Произошла ошибка на сервере.",
         });
       }
     });
@@ -24,77 +32,110 @@ const createCard = (req, res) => {
 
 const getCards = (req, res) => {
   Card.find({})
-    .populate(['owner', 'likes'])
+    .populate(["owner", "likes"])
     .then((cards) => {
       res.send(cards);
     })
     .catch(() => {
-      res.status(500).send({
-        message: 'Произошла ошибка на сервере.',
+      res.status(serverErrorStatus).send({
+        message: "Произошла ошибка на сервере.",
       });
     });
 };
 
 const deleteCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card.findByIdAndRemove(req.params.cardId)
-      .then((card) => {
-        if (!card) {
-          res.status(404).send({ message: 'Карточка с таким _id не найдена' });
-        } else {
-          res.send({ message: 'Карточка удалена' });
-        }
-      })
-      .catch(() => {
-        res.status(404).send({ message: 'Карточки с таким _id нет' });
-      });
-  } else {
-    res.status(400).send({ message: 'Невалидный _id карточки' });
-  }
+  // if (req.params.cardId.length === 24) {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (!card) {
+        res
+          .status(notFoundStatus)
+          .send({ message: "Карточка с таким _id не найдена" });
+      } else {
+        res.send({ message: "Карточка удалена" });
+      }
+    })
+    .catch((error) => {
+      if (error.name === "CastError") {
+        res
+          .status(badRequestStatus)
+          .send({ message: "Некорректный _id карточки" });
+      } else {
+        res
+          .status(serverErrorStatus)
+          .send({ message: "Карточки с таким _id нет" });
+      }
+    });
+  // } else {
+  //   res.status(badRequestStatus).send({ message: "Невалидный _id карточки" });
+  // }
 };
 
 const likeCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    )
-      .populate(['owner', 'likes'])
-      .then((card) => {
-        if (!card) {
-          res.status(404).send({ message: 'Карточка с таким _id не найдена' });
-          return;
-        }
-        res.send(card);
-      })
-      .catch(() => res.status(404).send({ message: 'Карточка с таким _id не найдена!!!!' }));
-  } else {
-    res.status(400).send({ message: 'Невалидный _id карточки' });
-  }
+  // if (req.params.cardId.length === 24) {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .populate(["owner", "likes"])
+    .then((card) => {
+      if (!card) {
+        res
+          .status(notFoundStatus)
+          .send({ message: "Карточка с таким _id не найдена" });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((error) => {
+      if (error.name === "CastError") {
+        res
+          .status(badRequestStatus)
+          .send({ message: "Некорректный _id карточки" });
+      } else {
+        res
+          .status(serverErrorStatus)
+          .send({ message: "Карточка с таким _id не найдена!!!!" });
+      }
+    });
+
+  // } else {
+  //   res.status(badRequestStatus).send({ message: "Невалидный _id карточки" });
+  // }
 };
 
 const dislikeCard = (req, res) => {
-  if (req.params.cardId.length === 24) {
-    Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $pull: { likes: req.user._id } },
-      { new: true },
-    )
-      .populate(['owner', 'likes'])
-      .then((card) => {
-        if (!card) {
-          res.status(404).send({ message: 'Карточка с таким _id не найдена' });
-          return;
-        }
-        res.send(card);
-      })
-      .catch(
-        res.status(404).send({ message: 'Карточка с таким _id не найдена' }),
-      );
-  } else {
-    res.status(400).send({ message: 'Невалидный _id карточки' });
-  }
+  // if (req.params.cardId.length === 24) {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .populate(["owner", "likes"])
+    .then((card) => {
+      if (!card) {
+        res
+          .status(notFoundStatus)
+          .send({ message: "Карточка с таким _id не найдена" });
+        return;
+      }
+      res.send(card);
+    })
+    .catch((error) => {
+      if (error.name === "CastError") {
+        res
+          .status(badRequestStatus)
+          .send({ message: "Некорректный _id карточки" });
+      } else {
+        res
+          .status(serverErrorStatus)
+          .send({ message: "Карточка с таким _id не найдена" });
+      }
+    });
+  // } else {
+  //   res.status(badRequestStatus).send({ message: "Невалидный _id карточки" });
+  // }
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,29 +1,46 @@
-const User = require('../models/user');
+const User = require("../models/user");
+const {
+  badRequestStatus,
+  notFoundStatus,
+  serverErrorStatus,
+} = require("../utils/constants");
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() =>
+      res
+        .status(serverErrorStatus)
+        .send({ message: "На сервере произошла ошибка" })
+    );
 };
 
 module.exports.getUserById = (req, res) => {
-  if (req.params.userId.length === 24) {
-    User.findById(req.params.userId)
-      .then((user) => {
-        if (!user) {
-          res
-            .status(404)
-            .send({ message: 'Пользователь по указанному _id не найден.' });
-          return;
-        }
-        res.send(user);
-      })
-      .catch(() => res
-        .status(404)
-        .send({ message: 'Пользователь по указанному _id не найден.' }));
-  } else {
-    res.status(400).send({ message: 'Некорректный _id' });
-  }
+  // if (req.params.userId.length === 24) {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(notFoundStatus)
+          .send({ message: "Пользователь по указанному _id не найден." });
+        return;
+      }
+      res.send(user);
+    })
+    .catch((error) => {
+      if (error.name === "CastError") {
+        res
+          .status(badRequestStatus)
+          .send({ message: "Некорректный _id карточки" });
+      } else {
+        res
+          .status(serverErrorStatus)
+          .send({ message: "Пользователь по указанному _id не найден." });
+      }
+    });
+  // } else {
+  //   res.status(400).send({ message: "Некорректный _id" });
+  // }
 };
 
 module.exports.addUser = (req, res) => {
@@ -31,55 +48,57 @@ module.exports.addUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
     .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res.status(400).send({ message: error.message });
+      if (error.name === "ValidationError") {
+        res.status(badRequestStatus).send({ message: error.message });
       } else {
-        res.status(500).send({ message: 'На сервере произошла ошибка' });
+        res
+          .status(serverErrorStatus)
+          .send({ message: "На сервере произошла ошибка" });
       }
     });
 };
 
 module.exports.editUserData = (req, res) => {
   const { name, about } = req.body;
-  if (req.user._id) {
-    User.findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      { new: 'true', runValidators: true },
-    )
-      .then((user) => res.send(user))
-      .catch((error) => {
-        if (error.name === 'ValidationError') {
-          res.status(400).send({ message: error.message });
-        } else {
-          res
-            .status(404)
-            .send({ message: 'Пользователь по указанному _id не найден.' });
-        }
-      });
-  } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
-  }
+  // if (req.user._id) {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: "true", runValidators: true }
+  )
+    .then((user) => res.send(user))
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        res.status(badRequestStatus).send({ message: error.message });
+      } else {
+        res
+          .status(serverErrorStatus)
+          .send({ message: "Пользователь по указанному _id не найден." });
+      }
+    });
+  // } else {
+  //   res.status(500).send({ message: "На сервере произошла ошибка" });
+  // }
 };
 
 module.exports.editUserAvatar = (req, res) => {
-  if (req.user._id) {
-    User.findByIdAndUpdate(
-      req.user._id,
-      { avatar: req.body.avatar },
-      { new: 'true', runValidators: true },
-    )
-      .then((user) => res.send(user))
-      .catch((error) => {
-        if (error.name === 'ValidationError') {
-          res.status(400).send({ message: error.message });
-        } else {
-          res
-            .status(404)
-            .send({ message: 'Пользователь по указанному _id не найден.' });
-        }
-      });
-  } else {
-    res.status(500).send({ message: 'На сервере произошла ошибка' });
-  }
+  // if (req.user._id) {
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: req.body.avatar },
+    { new: "true", runValidators: true }
+  )
+    .then((user) => res.send(user))
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        res.status(badRequestStatus).send({ message: error.message });
+      } else {
+        res
+          .status(serverErrorStatus)
+          .send({ message: "Пользователь по указанному _id не найден." });
+      }
+    });
+  // } else {
+  //   res.status(500).send({ message: "На сервере произошла ошибка" });
+  // }
 };
