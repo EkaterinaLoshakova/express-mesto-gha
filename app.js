728b94dc4c9e633c7582a0dfc5cbac031dcbba7c
Ekaterina Loshakova <katyushka8905@yandex.ru>
2023-08-20 16:21:39 +0300

    3com

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const bodyParser = require("body-parser");
-const { errors } = require("celebrate");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 // const { notFoundStatus } = require('./utils/constants');
-const auth = require("./middlewares/auth");
-const NotFoundError = require("./errors/NotFoundError");
+const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/NotFoundError');
 
 const app = express();
 
@@ -20,19 +20,20 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/mesto");
+mongoose.connect('mongodb://127.0.0.1:27017/mesto');
 
 const { PORT = 3000 } = process.env;
+
+app.use('/signup', require('./routes/signup'));
+app.use('/signin', require('./routes/signin'));
+
 app.use(auth);
-app.use("/signup", require("./routes/signup"));
-app.use("/signin", require("./routes/signin"));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
-app.use("/users", require("./routes/users"));
-app.use("/cards", require("./routes/cards"));
-
-app.use("*", (req, res, next) => {
+app.use('*', (req, res, next) => {
   // res.status(notFoundStatus).send({ message: "Такой старницы не существует" });
-  next(new NotFoundError("Такой старницы не существует"));
+  next(new NotFoundError('Такой старницы не существует'));
 });
 
 app.use(errors());
@@ -42,7 +43,7 @@ app.use((error, req, res) => {
 
   res.status(statusCode).send({
     // проверяем статус и выставляем сообщение в зависимости от него
-    message: statusCode === 500 ? "На сервере произошла ошибка" : message,
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
 });
 
